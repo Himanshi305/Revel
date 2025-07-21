9cab1ec78f0cce87fd0311838cc6d9a307ce407f
Himanshi <himanshigupta10425@gmail.com>
2025-07-21 17:04:21 +0530

    chore: initialized repo

---
+++ src/api/news.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=marvel+movies&language=en&sortBy=publishedAt&apiKey=2b2ef38f8a2644be865ce92abd45f9ff`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+}
