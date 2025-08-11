bf8a8f8e2a7492b428d4871cb63f927ca0d37305
Himanshi <himanshigupta10425@gmail.com>
2025-08-11 16:00:59 +0530

    chore: initialized repo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <div className=" top-0 z-50" style={{ fontFamily: 'Akira, sans-serif' }}>
       <div className="revel w-10 absolute top-5 left-2 md:left-5">
-        <img className="" src="./REVEL.png" alt="" />
+        <img src="./REVEL.png" alt="" />
       </div>
-      <nav className="absolute top-8 left-15 md:left-115 w-90 md:w-180 gap-5 md:gap-10 flex rounded-full shadow-lg">
+      <nav className="absolute top-8 left-15 md:left-1/4 w-90 md:w-180 gap-5 md:gap-10 flex rounded-full shadow-lg">
         <motion.div whileHover={{scale:1.2}} className="home text-sm md:text-2xl px-2 md:px-10">
           <Link to="/" className="text-black">
             Home
